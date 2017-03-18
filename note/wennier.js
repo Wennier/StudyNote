a067f99d2f2bd4f966abda550d8c0c1a453299b3
ZhangQuandan <2769942305@qq.com>
2017-03-18 09:52:50 +0800

    studynote

---
+++ note/wennier.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Wennier_R on 2017/3/18.
+ */
+
+function $( v ){
+    if(typeof v === 'function'){
+        window.onload = v;
+    }else if(typeof v === 'string'){
+        return document.getElementById(v);
+    }else if(typeof v=== "object"){
+        return v;
+    }
+};
