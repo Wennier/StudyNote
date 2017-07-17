a6de338cea4d00a4c8db619247cb9e2637f20107
ZhangQuandan <2769942305@qq.com>
2017-07-17 19:58:54 +0800

    canvas clock

---
+++ note/SummaryDaily/Day5/clock.js
@@ -0,0 +1,17 @@
+var dom = document.getElementById('clock');
+var ctx = dom.getContext('2d');
+var width = ctx.canvas.width;
+var height = ctx.canvas.height;
+var r = width/2;
+
+function drawBackground(){
+	ctx.translate(r,r);
+	ctx.beginPath();
+	ctx.lineWidth = 10;
+	ctx.arc(0,0,r-5,0,2*Math.PI,false);
+	ctx.stroke();
+
+
+}
+
+drawBackground();
